a6dd16f004f1e67499bb4118adc40a3c2faf3da8
Diego Silva <silva.pucrs@gmail.com>
2020-06-30 14:19:18 -0300

    chore: add gitignore file

---
+++ decrypt_secret.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./config/.env.dev ./config/.env.dev.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output ./config/.env.prod ./config/.env.prod.gpg
